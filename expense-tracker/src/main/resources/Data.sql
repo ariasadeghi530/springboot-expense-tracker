25a9d3d3badd8710944f31ac128f895dd6476ce4
Aria Sadeghi <ariasadeghi@gmail.com>
2020-06-26 13:16:14 -0700

    created backend, ready for testing

---
+++ expense-tracker/src/main/resources/Data.sql
@@ -0,0 +1,12 @@
+
+insert into user values (1, "Aria", "ariasadeghi@gmail.com")
+insert into user values (2, "Tim", "tim@email.com")
+insert into user values (3, "Bob", "bob@email.com")
+
+insert into category values(1, 'Travel')
+insert into category values(2, 'Auto Loan')
+insert into category values(3, 'Bills')
+
+insert into expense values (100, "New York", "2020-06-16T17:00:00.000Z",1,1)
+insert into expense values (101, "Porsche 911 GT3", "2020-07-16T17:00:00.000Z",2,2)
+insert into expense values (102, "SDGE", "2020-08-16T17:00:00.000Z",3,3)
