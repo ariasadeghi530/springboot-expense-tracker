59f18cfc4983482d5bda948a54d8834e0055dfae
Aria Sadeghi <ariasadeghi@gmail.com>
2020-06-26 21:27:19 -0700

    database init not working, but rest works

--- expense-tracker/src/main/java/com/resources/Data.sql
+++ expense-tracker/src/main/java/com/resources/Data.sql
@@ -1,22 +1,2 @@
-insert into users 
-values
-    (1, "Aria", "ariasadeghi@gmail.com"),
-    (2, "Tim", "tim@email.com"),
-    (3, "Bob", "bob@email.com")
 
-insert into category
-values(1, 'Travel')
-insert into category
-values(2, 'Auto Loan')
-insert into category
-values(3, 'Bills')
-
-insert into expense
-values
-    (100, "New York", "2020-06-16T17:00:00.000Z", 1, 1)
-insert into expense
-values
-    (101, "Porsche 911 GT3", "2020-07-16T17:00:00.000Z", 2, 2)
-insert into expense
-values
-    (102, "SDGE", "2020-08-16T17:00:00.000Z", 3, 3)
+insert into users values (1, 'Aria', 'ariasadeghi@email.com');
